bad6d6f54396d5253e483633c963fbbe54bcd62f
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-14 20:43:45 -0700

    Implemented permutationSort and check

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function permutationSort(a) {
-    return 0;
+    var iterations = 0;
+    if (a.length == 0) {
+        return iterations;
+    }
+    function permutation(a, firstE) {
+        if (check(a) == true) {
+            return iterations;
+        }
+        else {
+            for (var secondE = firstE; secondE < a.length; secondE++) {
+                iterations++;
+                [a[firstE], a[secondE]] = [a[secondE], a[firstE]];
+                permutation(a, firstE + 1);
+                if (check(a) == true) {
+                    return iterations;
+                }
+                [a[secondE], a[firstE]] = [a[firstE], a[secondE]];
+            }
+        }
+    }
+    permutation(a, 0);
+    return iterations;
+}
+
+function check(a) {
+    for (var n = 0; n < a.length; n++) {
+        if (a[n] < a[n - 1]) {
+            return false;
+        }
+    }
+    return true;
 }
